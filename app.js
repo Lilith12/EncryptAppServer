8627d0cf56a4e9cdbec840d9290193c5b8274937
Aleksandra Wichert <aleksandra.wichert@o2.pl>
2018-08-19 15:30:25 +0200

    fixes, added new features

--- app.js
+++ app.js
@@ -11,142 +11,177 @@ var users = require('./routes/users');
 var app = express();
 var http = require('http').Server(app);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 var io = require('socket.io')(http);
 var clients = {};
 var clientsUsernames = [];
 var usersSocket = {};
+var usersPublicKeys = {};
 var rooms = {};
 var roomsNames = [];
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname+'/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function (socket) {
-  console.log('user connected');
-  socket.on('set username', function (username) {
-    console.log(username);
-    if (clients[username] == undefined) {
-      socket.username = username;
-      clients[username] = socket.id;
-      usersSocket[socket.id] = username;
-      clientsUsernames.push(username);
-      socket.emit('arrayOfUsers', {"usersSocket": usersSocket});
-    } else if (clients[username] === socket.id) {
-      // Ignore for now
-    } else {
-      usernameAlreadyInUse(socket, username);
-    }
-  });
+    console.log('user connected');
+    socket.on('set username', function (username, publicKey) {
+        console.log(username);
+        if (clients[username] === undefined) {
+            socket.username = username;
+            socket.publicKey = publicKey;
+            clients[username] = socket.id;
+            usersPublicKeys[socket.id] = publicKey;
+            usersSocket[socket.id] = username;
+            clientsUsernames.push(username);
+            socket.emit('arrayOfUsers', {"usersSocket": usersSocket});
+            socket.emit('usersArray', {"users": clientsUsernames});
+        } else if (clients[username] === socket.id) {
+            // Ignore for now
+        } else {
+            usernameAlreadyInUse(socket.id);
+        }
+    });
 
- socket.on('disconnect user', function (username) {
-     console.log("user disconnected " + username);
-     var userSocket = clients[username];
-     delete clients[username];
-     delete usersSocket[userSocket];
-     var index = clientsUsernames.indexOf(username);
-     clientsUsernames.splice(index, 1);
- });
+    socket.on('disconnect user', function (username) {
+        console.log("user disconnected " + username);
+        var userSocket = clients[username];
+        delete clients[username];
+        delete usersSocket[userSocket];
+        var index = clientsUsernames.indexOf(username);
+        clientsUsernames.splice(index, 1);
+    });
 
-  socket.on('get rooms', function (){
-    socket.emit('roomsArray', {"rooms": roomsNames});
-  });
+    socket.on('get rooms', function () {
+        socket.emit('roomsArray', {"rooms": roomsNames});
+    });
 
-  socket.on('create room', function (roomName) {
-    var newRoom = [];
-    roomsNames.push(roomName);
-    rooms[roomName] = newRoom;
-  });
+    socket.on('create room', function (roomName) {
+        var newRoom = [];
+        roomsNames.push(roomName);
+        rooms[roomName] = newRoom;
+    });
 
 
-  socket.on('connect to room', function (roomName) {
-    var roomUsers = rooms[roomName];
-    if(roomUsers.indexOf(socket.id) == -1)
-      rooms[roomName].push(socket.id);
-      roomUsers.forEach(function(entry) {
-          io.to(entry).emit('user connected', {username: socket.username});
-      });
-  });
+    socket.on('connect to room', function (roomName) {
+        var roomUsers = rooms[roomName];
+        console.log(roomUsers);
+        console.log(roomUsers.indexOf(socket.id));
+        let user = {user: socket.id, username: socket.username, key: socket.publicKey};
+        if (roomUsers.find(roomUser => JSON.stringify(roomUser) === JSON.stringify(user)) === undefined) {
+            rooms[roomName].push(user);
+            roomUsers.forEach(function (entry) {
+                io.to(entry.user).emit('user connected', {username: socket.username});
+            });
+        }
+    });
 
-  socket.on('disconnect from room', function(roomName){
-      var index = rooms[roomName].indexOf(socket.id);
-      rooms[roomName].splice(index, 1);
-      var roomUsers = rooms[roomName];
-      roomUsers.forEach(function(entry) {
-          io.to(entry).emit('user disconnected', {username: socket.username});
-      });
-  });
+    socket.on('disconnect from room', function (roomName) {
+        rooms[roomName] = rooms[roomName].filter(function( obj ) {
+            return obj.user !== socket.id;
+        });
+        console.log("disconnected");
+        var roomUsers = rooms[roomName];
+        if(rooms[roomName].length > 0){
+            roomUsers.forEach(function (entry) {
+                io.to(entry.user).emit('user disconnected', {username: socket.username});
+            });
+        } else {
 
-  socket.on('get users', function (){
-    socket.emit('usersArray', {"userssSocket": clientsUsernames});
-  });
+        }
+    });
 
-  socket.on('typing PW', function (toClient) {io.to(clients[toClient]).emit('typing', {username: socket.username});
-  });
+    socket.on('get users', function () {
+        socket.emit('usersArray', {"users": clientsUsernames});
+    });
 
-  socket.on('stop typing PW', function (toClient) {
-    io.to(clients[toClient]).emit('stop typing', {username: socket.username});
-  });
+    socket.on('typing PW', function (toClient) {
+        io.to(clients[toClient]).emit('typing', {username: socket.username});
+    });
 
-  socket.on('new message', function (toClient, data, wasEdited, position, messageCode) {
-    var dataToEmit = {username: socket.username, message: data, wasEdited: wasEdited, position: position, messageCode: messageCode};
-    io.to(clients[toClient]).emit('pwMessage', dataToEmit);
-    io.to(clients[toClient]).emit('pwMessageGlobal', dataToEmit);
-  });
+    socket.on('stop typing PW', function (toClient) {
+        io.to(clients[toClient]).emit('stop typing', {username: socket.username});
+    });
 
-  socket.on('new group message', function (roomName, data) {
-      var roomUsers = rooms[roomName];
-      roomUsers.forEach(function(entry) {
-          if(entry!=clients[socket.username]){
-              var dataToEmit = {roomName: roomName, username: socket.username, message: data};
-              io.to(entry).emit('groupMessage', dataToEmit);
-              io.to(entry).emit('groupMessageGlobal', dataToEmit);
-          }
-      });
-  });
+    socket.on('new message', function (toClient, data, wasEdited, position, messageCode) {
+        var dataToEmit = {
+            username: socket.username,
+            message: data,
+            wasEdited: wasEdited,
+            position: position,
+            messageCode: messageCode
+        };
+        io.to(clients[toClient]).emit('pwMessage', dataToEmit);
+        io.to(clients[toClient]).emit('pwMessageGlobal', dataToEmit);
+    });
 
-  socket.on('typing to group', function (roomName) {
-      var roomUsers = rooms[roomName];
-      roomUsers.forEach(function(entry) {
-          if (entry != clients[socket.username])
-              io.to(entry).emit('groupTyping', {roomName: roomName, username: socket.username});
-      });
-  });
+    socket.on('new group message', function (toClient, roomName, data, wasEdited, position, messageCode) {
+        let roomUsers = rooms[roomName];
+        console.log(roomName, data, wasEdited, position, messageCode);
+            if (toClient !== socket.username) {
+                let dataToEmit = {
+                    roomName: roomName,
+                    username: socket.username,
+                    message: data,
+                    wasEdited: wasEdited,
+                    position: position,
+                    messageCode: messageCode
+                };
+                io.to(clients[toClient]).emit('groupMessage', dataToEmit);
+                io.to(clients[toClient]).emit('groupMessageGlobal', dataToEmit);
+            }
+    });
 
-  socket.on('stop typing to group', function (roomName) {
-      var roomUsers = rooms[roomName];
-      roomUsers.forEach(function(entry) {
-          if (entry != clients[socket.username])
-              io.to(entry).emit('stopGroupTyping', {roomName: roomName, username: socket.username});
-      });
-  });
+    socket.on('typing to group', function (roomName) {
+        let roomUsers = rooms[roomName];
+        roomUsers.forEach(function (entry) {
+            if (entry.user !== clients[socket.username])
+                io.to(entry.user).emit('groupTyping', {roomName: roomName, username: socket.username});
+        });
+    });
 
+    socket.on('stop typing to group', function (roomName) {
+        let roomUsers = rooms[roomName];
+        roomUsers.forEach(function (entry) {
+            if (entry.user !== clients[socket.username])
+                io.to(entry.user).emit('stopGroupTyping', {roomName: roomName, username: socket.username});
+        });
+    });
+
+    socket.on('request public key', function (fromUser) {
+        let fromUserSocket = clients[fromUser];
+        io.to(clients[socket.username]).emit('requestedPublicKey', {publicKey: usersPublicKeys[fromUserSocket]});
+    });
+
+    socket.on('request users public keys', function (roomName) {
+        let roomUsers = rooms[roomName];
+        io.to(clients[socket.username]).emit('usersPublicKeys', roomUsers);
+    });
+
+    checkIfUserIsActive(socket, io);
 });
 
 
 http.listen(8085, function () {
-  console.log('8085');
+    console.log('8085');
 });
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -154,30 +189,35 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
+function checkIfUserIsActive(socket, io) {
+    setInterval(function () {
+        io.emit('usersArray', {"users": clientsUsernames});
+    }, 15000);
+}
 
 module.exports = app;
 
-function usernameAlreadyInUse(socket, uName) {
-  setTimeout(function () {
-    socket.emit('usernameUsed', {"usernameInUse": true});
-  }, 500);
+function usernameAlreadyInUse(id) {
+    setTimeout(function () {
+        io.to(id).emit('usernameUsed', {"usernameInUse": true});
+    }, 500);
 }
